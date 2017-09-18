0b3f21be22fda7237151452c5d441904e9414ef2
Eric Persson <j.eric.persson@icloud.com>
2017-09-19 08:40:57 +0900

    Add source code for cleaning up enron data outliers. Last part of outlier mini-project.

--- mini-projects/07_outliers/enron_outliers.py
+++ mini-projects/07_outliers/enron_outliers.py
@@ -7,13 +7,20 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+# read in data dictionary, convert to numpy array
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl",
+                             "r"))
 features = ["salary", "bonus"]
+
+# your code below
+data_dict.pop("TOTAL")
 data = featureFormat(data_dict, features)
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter(salary, bonus)
 
-### your code below
-
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
