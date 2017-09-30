39d3b0aad6eda45f5c34eedde2cd39a4005023d6
Eric Persson <j.eric.persson@icloud.com>
2017-09-30 21:47:50 +0900

    Add source code and pickle data files for text learning mini-project.

--- mini-projects/09_text_learning/vectorize_text.py
+++ mini-projects/09_text_learning/vectorize_text.py
@@ -22,7 +22,6 @@ from parse_out_email_text import parseOutText
     The data is stored in lists and packed away in pickle files at the end.
 """
 
-
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
@@ -41,21 +40,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            ignored_words = ["sara", "shackleton", "chris", "germani"]
+            for ignored_word in ignored_words:
+                text = text.replace(ignored_word, "")
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -67,9 +74,13 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
+cvec = CountVectorizer(stop_words='english')
+cvec.fit(word_data)
+
+print len(cvec.get_feature_names())
+print cvec.get_feature_names()[34597]
