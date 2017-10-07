76d5050c1dab978dac653d32efb977a88d9f36db
Eric Persson <j.eric.persson@icloud.com>
2017-10-07 14:48:58 +0900

    Add code for the evaluation mini-project.

---
+++ mini-projects/13_evaluation_metrics/evaluate_poi.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.model_selection import train_test_split
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+### it's all yours from here forward!  
+
+# Split the data into a test and a training data set.
+labels_train, labels_test, features_train, features_test = train_test_split(
+    labels, features, test_size=0.3, random_state=42)
+
+# Create a simple DecisionTreeClassifier and fit it to our data.
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+# Use the fitted model to predict and calculate the accuracy score.
+pred = clf.predict(features_test)
+
+print "Accuracy of the classifier: "
+print accuracy_score(labels_test, pred)
+print "POI's found by the classifier: "
+print len(pred[pred == 1])
+print "Total number of people in the data set: "
+print len(pred)
+print "Compare the predited labels to the test data set (pred, test): "
+print zip(pred, labels_test)
+print "Precision score for the classifier: "
+print precision_score(labels_test, pred)
+print "Recall score for the classifier: "
+print recall_score(labels_test, pred)
+
